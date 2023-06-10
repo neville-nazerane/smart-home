549a3d1ac32571aa5db75a2636af35a5197aff82
Neville Nazerane <live.neville@live.com>
2023-06-10 15:43:30 -0700

    bond client

---
+++ src/SmartHome.ServerServices/Clients/BondClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.ServerServices.Clients
+{
+    public class BondClient
+    {
+        private readonly HttpClient _client;
+
+        public BondClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+    }
+}
