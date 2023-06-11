89476b003bca422be8265abff05206c63f91b7b0
Neville Nazerane <live.neville@live.com>
2023-06-10 19:40:41 -0700

    bond setup

---
+++ src/SmartHome.ServerServices/Clients/BondClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.ServerServices.Clients
+{
+    public class BondClient
+    {
+        private readonly HttpClient _client;
+
+        public BondClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public static HttpClient SetupClient(HttpClient client,
+                                             string baseUrl,
+                                             string token)
+        {
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Add("BOND-Token", token);
+            return client;
+        }
+
+
+    }
+}
