7bd902513876cb2683980ea8d32832cf10510d2a
Neville Nazerane <live.neville@live.com>
2023-06-16 22:43:29 -0700

    fixed migrations

--- src/SmartHome.ServerServices/AppDbContext.cs
+++ src/SmartHome.ServerServices/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SmartHome.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,9 @@ namespace SmartHome.ServerServices
 {
     public class AppDbContext : DbContext
     {
+
+        public DbSet<DeviceLog> DeviceLogs { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
