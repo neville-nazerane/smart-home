2f59b315f54312fe69452d10a4939f9ffb78788b
Neville Nazerane <live.neville@live.com>
2023-06-16 21:44:23 -0700

    listener logic

---
+++ src/SmartHome.Models/Extensions/ListenableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Models.Extensions
+{
+    public static class ListenableExtensions
+    {
+
+        public static bool Is(this IListenableDevice device, ListenedDevice listened)
+            => device.DeviceType == listened.DeviceType && device.Id == listened.Id;
+
+    }
+}
