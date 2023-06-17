5137705131535675af6410873a016ee3995b30aa
Neville Nazerane <live.neville@live.com>
2023-06-17 10:43:11 -0700

    smart devices custom

---
+++ src/SmartHome.Models/SmartDevices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmartHome.Models.SmartContextBase;
+
+namespace SmartHome.Models
+{
+    public partial class SmartDevices
+    {
+        private readonly SmartContextBase _context;
+
+        public SmartDevices(SmartContextBase context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ src/SmartHome.Models/SmartDevices.custom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Models
+{
+    public partial class SmartDevices
+    {
+
+        const string CLOSET_MOTION_SENSOR_ID = "";
+
+
+
+    }
+}
